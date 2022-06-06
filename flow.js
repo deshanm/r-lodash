b9b0600e40724a1c05a31cd743d256430b16b957
deshanm <madurajithdeshan@gmail.com>
2022-06-06 22:19:55 +0530

    bug-version: re-applied the bug codebase

--- flow.js
+++ flow.js
@@ -21,7 +21,7 @@
  * // => 9
  */
 function flow(...funcs) {
-  const length = funcs.length
+  const length = funcs ? funcs.length : 0
   let index = length
   while (index--) {
     if (typeof funcs[index] !== 'function') {
