28bbafe554d6cbd0425043d1ee3c3ba43c2c7ad6
deshanm <madurajithdeshan@gmail.com>
2022-06-06 22:49:57 +0530

    ts-version: changed file to typescript

--- after.ts
+++ after.ts
@@ -15,11 +15,11 @@
  * forEach(saves, type => asyncSave({ 'type': type, 'complete': done }))
  * // => Logs 'done saving!' after the two async saves have completed.
  */
-function after(n, func) {
+function after(n: number, func: Function) {
   if (typeof func !== 'function') {
     throw new TypeError('Expected a function')
   }
-  return function(...args) {
+  return function(...args: any[]) {
     if (--n < 1) {
       return func.apply(this, args)
     }
